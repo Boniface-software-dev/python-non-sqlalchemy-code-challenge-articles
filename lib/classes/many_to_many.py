715ee6505a7daa4d662a1c172deb7f4e1b93db7b
Boniface Kimani Muguro <mmbonifacekimani@gmail.com>
2025-05-30 12:35:56 +0300

    relatonships and Traking

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,10 +1,17 @@
 class Article:
+    _all = []
     def __init__(self, author, magazine, title):
         if not isinstance(title, str) or not (5 <= len(title) <=50):
             raise ValueError("Title must be a string between 5 and 50 characters.")
         self.author = author
         self.magazine = magazine
         self.title = title
+        Article._all.append(self)
+
+    @classmethod
+    def all(cls):
+        """Returns a list of all articles."""
+        return cls._all
         
 class Author:
     def __init__(self, name):
@@ -17,17 +24,19 @@ class Author:
         return self._name
   
     def articles(self):
-        pass
+        return [a for a in Article.all() if a.author == self]
 
     def magazines(self):
-        pass
+        return list(a.magazine for a in self.articles())
 
     def add_article(self, magazine, title):
-        pass
+        return Article(self, magazine, title)
 
     def topic_areas(self):
-        pass
-    
+        if not self.articles():
+            return None
+        return list({m.category for m in self.magazines()})
+
 class Magazine:
     def __init__(self, name, category):
         self.name = name
@@ -53,13 +62,23 @@ class Magazine:
         self._category = value
 
     def articles(self):
-        pass
+        return [a for a in Article.all() if a.magazine == self]
 
     def contributors(self):
-        pass
+        return list({a.author for a in self.articles()})
 
     def article_titles(self):
-        pass
+        if not self.articles():
+            return None
+        return [a.title for a in self.articles()]
 
     def contributing_authors(self):
-        pass
+        authors = [a.author for a in self.articles()]
+        return [a for a in set (authors) if authors.count(a) > 2] or None
+
+    @classmethod
+    def top_publisher(cls):
+        magazines = {a.magazines: 0 for a in Article.all()}
+        for artile in Article.all():
+            magazines[article.magazine] += 1
+        return max(magines.items(), key=lambda x: x[1])[0] if magazines else None
